aeae7bc0787198c01b6de9557eb7ae73e2a42e4e
sajdeen <sajdeen0786@gmail.com>
2025-07-16 03:20:47 +0530

    commit

--- script.js
+++ script.js
@@ -1,37 +1,48 @@
-
 const inputBox = document.getElementById("taskInput");
-const button = document.getElementById("addTask");
+const addedTaskBtn = document.getElementById("addTask");
 const todoList = document.getElementById("todoList");
+const taskContainer = document.getElementById("task-container");    
 
-button.addEventListener("click", () => {
-    const inputText = inputBox.value.trim();
-
-    if (inputText !== "") {
-        const li = document.createElement("li");
-        li.textContent = inputText;
-
-        // Toggle completed status on click
-        li.addEventListener("click", () => {
-            li.classList.toggle("completed");
-        });
-
-
-        
-        // Create a remove button (X)
-        const removeBtn = document.createElement("button");
-        removeBtn.textContent = "X";
-        removeBtn.className = "remove-btn";
-
-        // Remove task on clicking X
-        removeBtn.addEventListener("click", (e) => {
-            e.stopPropagation(); // prevent triggering li click
-            li.remove();
-        });
-
-        li.appendChild(removeBtn);
-        todoList.appendChild(li);
-        inputBox.value = ""; // Clear input
-    } else {
-        alert("Please enter a task!");
+taskContainer.style.display = "none";
+addedTaskBtn.addEventListener("click", function() {
+    const task = inputBox.value.trim();
+    if (!task) {
+        alert("Please enter a task");
+        return;
     }
+
+taskContainer.style.display ="block";
+const li = document.createElement("li");
+li.textContent = task;
+ li.style.display = "flex";
+li.style.justifyContent = "space-between";
+li.style.alignItems = "center";
+
+const deleteBtn = document.createElement("button");
+deleteBtn.textContent ="X";
+deleteBtn.className = "delete-btn";
+
+
+
+deleteBtn.style.backgroundColor ="red";
+deleteBtn.style.color = "white";
+deleteBtn.style.border = "none";
+deleteBtn.style.borderRadius = "50%";
+deleteBtn.style.cursor = "pointer";
+deleteBtn.style.padding = "0 5px";  
+deleteBtn.style.position = "relative";
+deleteBtn.style.fontSize = "16px";
+deleteBtn.style.marginLeft = "10vh";
+deleteBtn.style.right = "10px";
+deleteBtn.style.top = "40%";     
+
+deleteBtn.addEventListener("click",function(){
+    li.remove();
+});
+
+li.appendChild(deleteBtn);
+
+todoList.appendChild(li);
+inputBox.value ="";
+
 });
